7639330964788dcbde1d0db1a0c8341e01191b9f
Francimar <francimarvasconcelosmota3@gmail.com>
2024-09-28 14:22:23 -0300

    Exercicio com encapsulamento, conta de banco ainda nao acabado

---
+++ Exercicio005/src/aplicacao/Programa.java
@@ -0,0 +1,48 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Banco;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Banco banco = new Banco();
+
+		System.out.print("Digite o número da conta: ");
+		int conta = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Insira o titular da conta: ");
+		String nome = sc.nextLine();
+		
+		banco = new Banco(conta, nome);
+		
+		System.out.print("Existe um depósito inicial (s/n)? ");
+		char simNao = sc.next().charAt(0);
+		if (simNao == 'S') {
+			System.out.print("Insira o valor do depósito inicial: ");
+			double desposito = sc.nextDouble();
+			System.out.println();
+			System.out.println("Conta atual: ");
+			banco.Deposito(desposito);
+			System.out.println(banco);
+		}else {
+			System.out.println();
+			System.out.println("Conta atual: ");
+			System.out.println(banco);
+			System.out.println();
+		}
+			System.out.println();
+			System.out.print("Insira um valor de depósito: ");
+			double desposito = sc.nextDouble();
+			banco.Deposito(desposito);
+			System.out.println(banco);
+			sc.close();
+		}
+	}
+
---
+++ Exercicio005/src/entidade/Banco.java
@@ -0,0 +1,53 @@
+package entidade;
+
+public class Banco {
+
+	private int conta;
+	private String nome;
+	private double saldo;
+	
+	public Banco() {
+	
+	}
+	
+
+	public Banco(int conta, String nome) {
+		this.conta = conta;
+		this.nome = nome;
+	}
+
+
+	public void Deposito(double deposito) {
+		saldo += deposito;
+	}
+
+	public int getConta() {
+		return conta;
+	}
+
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String toString() {
+		return "Conta "+ conta + ", " + "Titular: " + nome + ", " + "Saldo " + saldo;
+	}
+
+	
+}
